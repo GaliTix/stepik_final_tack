60eebb4c6ed4ba861c6501491f40ac94e54f9c32
Galina Tikhomirova <g.tikhomirova@nicetu.spb.ru>
2022-03-11 15:24:41 +0300

    Реализация LoginPage раздел 4.2 шаг 11

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,16 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser,link) # инициализируем Page Object, передаём в конструктор экземпляр драйвера и url адрес
     page.open() # отркываем страницу
     page.go_to_login_page() # выполняем метод страницы - переходим на страницу логина
+    page = LoginPage(browser, link)
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
+
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
