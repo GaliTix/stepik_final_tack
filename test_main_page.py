9061f6b0fbed561ee4acae8e73e27565e60be20c
Galina Tikhomirova <g.tikhomirova@nicetu.spb.ru>
2022-03-11 15:59:27 +0300

    Поправила переходы между страницами раздел 4.2 шаг 12

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,16 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser,link) # инициализируем Page Object, передаём в конструктор экземпляр драйвера и url адрес
-    page.open() # отркываем страницу
-    page.go_to_login_page() # выполняем метод страницы - переходим на страницу логина
-    page = LoginPage(browser, link)
-    page.should_be_login_url()
-    page.should_be_login_form()
-    page.should_be_register_form()
+    page = MainPage(browser, link)  # инициализируем Page Object, передаём в конструктор экземпляр драйвера и url адрес
+    page.open()  # отркываем страницу
+    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
 
 
 def test_guest_should_see_login_link(browser):
