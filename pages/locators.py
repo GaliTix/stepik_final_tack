e318222f4b0060261b72fc9535ecb1d6bc620b02
Galina Tikhomirova <g.tikhomirova@nicetu.spb.ru>
2022-03-14 15:11:38 +0300

    Перенос методов go_to_login_page и should_be_login_link в класс BasePage раздел 4.3 шаг 9

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,5 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -15,4 +12,8 @@ class ProductPageLocators():
     MESSAGE_PRICE = (By.XPATH, '//div[@id="messages"]//p')
     PRICE_PRODUCT = (By.CSS_SELECTOR, 'p.price_color')
     PRICE_TO_BASKET = (By.XPATH, '//div[@class="basket-mini pull-right hidden-xs"]')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
--- test_product_page.py
+++ test_product_page.py
@@ -20,6 +20,13 @@ def test_add_product_to_basket(browser, substring):
     page.should_be_message_contain_price()
 
 
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -34,6 +41,13 @@ def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 @pytest.mark.xfail(reason="fixing this bug right now")
